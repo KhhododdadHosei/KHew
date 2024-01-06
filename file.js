71c76195f1d7a6f096324c8e7a544c17e32e1a6a
KhhododdadHosei <138739519+KhhododdadHosei@users.noreply.github.com>
2024-01-06 15:29:54 +0100

    Update file.js

--- file.js
+++ file.js
@@ -14,7 +14,7 @@ public static void main(String[] args) {
 
 public class Main {
   public static void main(String[] args) {
-    final int myNum = 126385;
+    final int myNum = 15;
     myNum = 20; // will generate an error
     System.out.println(myNum);
   }
